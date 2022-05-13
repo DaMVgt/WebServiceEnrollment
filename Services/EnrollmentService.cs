09a3f7e358ea6f8ce9f3369f7257d36a65dd157a
DaMVgt <dmonterroso098@outlook.com>
2022-05-13 16:14:05 -0600

    ⚰️ Code dead

--- Services/EnrollmentService.cs
+++ Services/EnrollmentService.cs
@@ -7,15 +7,10 @@ namespace WebServicesEnrollment.Services
     public class EnrollmentService : IEnrollmentService
     {
         private SqlConnection connection = new SqlConnection("Server=localhost;Database=kalum_test;User Id=sa;Password=Inicio.2022;");
-        private List<Aspirante> aspirantes = new List<Aspirante>();
         private List<CarreraTecnica> carreras = new List<CarreraTecnica>();
         private List<Cargo> cargos = new List<Cargo>();
         public EnrollmentService()
         {
-            aspirantes.Add(new Aspirante() { NoExpediente = "EXP-2022001", Apellidos = "Perez", Nombres = "Luis", Email = "lperez@gmail.com", Direccion = "Guatemala, Guatemala", Telefono = "123343" });
-            aspirantes.Add(new Aspirante() { NoExpediente = "EXP-2022002", Apellidos = "Mancilla", Nombres = "Raul", Email = "rmancilla@gmail.com", Direccion = "Guatemala, Guatemala", Telefono = "2391293" });
-            aspirantes.Add(new Aspirante() { NoExpediente = "EXP-2022003", Apellidos = "Lemus", Nombres = "Roberto", Email = "lroberto@gmail.com", Direccion = "Guatemala, Guatemala", Telefono = "1939481" });
-
             carreras.Add(new CarreraTecnica() { CarreraId = "1", Carrera = "FullStack DOTNET Core" });
             carreras.Add(new CarreraTecnica() { CarreraId = "2", Carrera = "FullStack Java EE" });
             carreras.Add(new CarreraTecnica() { CarreraId = "3", Carrera = "Desarrollo Movil Android" });
