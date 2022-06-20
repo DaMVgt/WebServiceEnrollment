4cffe52122d00634c8fd30d151a8636c17f0566c
DaMVgt <dmonterroso098@outlook.com>
2022-06-19 18:18:14 -0600

    ✅ Add: new  packages

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
+using System.Text;
 using Microsoft.AspNetCore;
+using Serilog;
 using WebServicesEnrollment;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        Log.Logger = new LoggerConfiguration().WriteTo.File("WebServicesEnrollment.out", Serilog.Events.LogEventLevel.Debug, "{Message:lj}{NewLine}", encoding: Encoding.UTF8).CreateLogger();
         CreateWebHostBuilder(args).Build().Run();
     }
 
